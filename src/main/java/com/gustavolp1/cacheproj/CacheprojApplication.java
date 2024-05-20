ea7a133eb73435e453351facbc7b4e3957320031
gustavolp1 <gustavolp1@al.insper.edu.br>
2024-05-20 20:39:13 -0300

    initial setup

---
+++ src/main/java/com/gustavolp1/cacheproj/CacheprojApplication.java
@@ -0,0 +1,13 @@
+package com.gustavolp1.cacheproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavolp1/cacheproj/CacheprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavolp1.cacheproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
